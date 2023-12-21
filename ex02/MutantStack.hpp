5c80eda7f77f70cba430580f286e730b98120a18
Mario Gonzalez galisteo <magonzal@c3r8s4.42madrid.com>
2023-12-21 21:56:45 +0100

    alan

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: magonzal <magonzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 17:55:24 by magonzal          #+#    #+#             */
-/*   Updated: 2023/11/30 17:55:25 by magonzal         ###   ########.fr       */
+/*   Updated: 2023/12/21 21:52:52 by magonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,59 +27,57 @@
 
 template<typename T>
 class MutantStack : public std::stack<T>
-{   
-    public:
-        MutantStack(void) : std::stack<T>() {}
-        MutantStack(const MutantStack &other) : std::stack<T>(other) {}
-        ~MutantStack(void) {}
+{
+	public:
+		MutantStack(void) : std::stack<T>() {}
+		MutantStack(const MutantStack &other) : std::stack<T>(other) {}
+		~MutantStack(void) {}
 
-        MutantStack &operator=(const MutantStack &other)
-        {
-            if(this != &other)
-                *this = other;
-            return *this;
-        }
+		MutantStack &operator=(const MutantStack &other)
+		{
+			if(this != &other)
+				*this = other;
+			return *this;
+		}
 		typedef typename std::stack<T>::container_type::iterator 				iterator;
 		typedef typename std::stack<T>::container_type::const_iterator			const_iterator;
 		typedef typename std::stack<T>::container_type::reverse_iterator		reverse_iterator;
 		typedef typename std::stack<T>::container_type::const_reverse_iterator	const_reverse_iterator;
 
-        iterator begin(void)
-        {
-            return this->c.begin();
-        };
-        const_iterator cbegin(void) const
-        {
-            return this->c.cbegin();
-        };
-        
-        iterator end(void)
-        {
-            return this->c.end();
-        };
-        const_iterator cend(void) const
-        {
-            return this->c.cend();
-        };
+		iterator begin(void)
+		{
+			return this->c.begin();
+		};
+		const_iterator cbegin(void) const
+		{
+			return this->c.cbegin();
+		};
+		
+		iterator end(void)
+		{
+			return this->c.end();
+		};
+		const_iterator cend(void) const
+		{
+			return this->c.cend();
+		};
 
-        reverse_iterator rbegin(void)
-        {
-            return this->c.rbegin();
-        };
-        const_reverse_iterator crbegin(void) const
-        {
-            return this->c.crbegin();
-        };
-
-
-        reverse_iterator rend(void)
-        {
-            return this->c.rend();
-        };
-        const_reverse_iterator crend(void) const
-        {
-            return this->c.crend();
-        };
+		reverse_iterator rbegin(void)
+		{
+			return this->c.rbegin();
+		};
+		const_reverse_iterator crbegin(void) const
+		{
+			return this->c.crbegin();
+		};
+		reverse_iterator rend(void)
+		{
+			return this->c.rend();
+		};
+		const_reverse_iterator crend(void) const
+		{
+			return this->c.crend();
+		};
 };
 
 #endif
